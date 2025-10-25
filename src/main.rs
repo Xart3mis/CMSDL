b42f576c5af10059d9431727ceeaff78ce8f013e
Yassin Diab <yassin.diab@student.giu-uni.de>
2025-10-25 18:07:57 +0300

    locate/fetch driver & manage process

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,29 @@
 mod fetcher;
-use fetcher::fetch_geckodriver;
+mod process;
+use std::{path::PathBuf, process::exit};
+
+use fetcher::{fetch_geckodriver, find_geckodriver};
+use process::ManagedProcess;
 
 fn main() {
-    println!("{}", fetch_geckodriver().unwrap().to_str().unwrap());
+    let driver: PathBuf;
+    if let Some(driver_) = find_geckodriver() {
+        driver = driver_;
+    } else if let Some(driver_) = fetch_geckodriver() {
+        driver = driver_;
+    } else {
+        eprintln!("Error: Could not fetch geckodriver! Exiting...");
+        exit(1);
+    }
+
+    let mut proc = ManagedProcess::start(driver, &["--headless"]).unwrap();
+    println!("Process started (PID: {:?})", proc.child.id());
+
+    //do something...
+    std::thread::sleep(std::time::Duration::from_secs(5));
+
+    if proc.is_running() {
+        println!("Killing Process");
+        proc.kill().unwrap();
+    }
 }
